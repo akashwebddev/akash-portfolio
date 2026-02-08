db973d892fbcd06de62c11f8e1c719905d88ae00
akashwebddev <akashwebddev@gmail.com>
2026-02-08 13:14:12 +0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Loader from "../components/Loader";
+import Hero from "../components/Hero";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Loader />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
