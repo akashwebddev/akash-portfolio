cce1e7d69edc1964e5dc42a4d59fd9a2a1791ee0
akashwebddev <akashwebddev@gmail.com>
2026-02-08 13:19:05 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: "#7c3aed",
+        cyan: "#22d3ee",
+      },
+    },
+  },
+  plugins: [],
+};
